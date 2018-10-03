b2aa10cac0d9348fddccdabbd8dec5109a5c6d21
Carlos <carloshernandezcrespo@gmail.com>
2018-10-03 23:20:45 +0200

    avance

--- app/src/main/java/com/example/carlos_hc/dogdate/MainActivity.java
+++ app/src/main/java/com/example/carlos_hc/dogdate/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.carlos_hc.dogdate;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import Objetos.Perro;
 
@@ -14,24 +25,84 @@ public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
 
+    //creamos una lista de perros donde guardaremos los resultados de las queries
+    List<Perro> perros = new ArrayList<>();
+    Perro miPerro;
+
+    Context miCOntexto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        miCOntexto = getApplicationContext();
 
 
-        //COMO ESCRIBIR EN FIREBASE BBDD
+        //CONECTAMOS A LA tabla "usuarios"
         mDatabase = FirebaseDatabase.getInstance().getReference("usuarios");
 
-        Perro churrete = new Perro(3,"churrete@gmail.com","macho","churrete", "chucho");
+//        //COMO CREAR UN NUEVO MIEMBRO EN LA BASE DE DATOS
+//        Perro churrete = new Perro(3,"churrete@gmail.com","macho","churrete", "chucho");
+//        mDatabase.child("47").setValue(churrete);
+
+//        //ACTUALIZAR UNA PROPIEDAD DE UN MIEMBRO
+//        mDatabase.child("46").child("genero").setValue("Macho");
 
 
-        mDatabase.child("46").setValue(churrete);
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                miPerro = dataSnapshot.child("1").getValue(Perro.class);
+
+                Log.i("TUSA",miPerro.getNombre());
+
+                Toast.makeText(miCOntexto,miPerro.getNombre(),Toast.LENGTH_LONG);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+        mDatabase.addValueEventListener(postListener);
 
 
-        Log.i("INFORMACION","Se ha hecho un add a la base de datos");
+
+        ///@@@@_QUERIES_@@@@
+        //Objeto que saca todos los hijos de la "BBDD" a la que apunta mDatabase
+//    ValueEventListener valueEventListener = new ValueEventListener()  {
+//        @Override
+//        public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//            perros.clear();
+//
+//
+//            if (dataSnapshot.exists()){
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                    Perro tmp = snapshot.getValue(Perro.class);
+//                    perros.add(tmp);
+//                }
+//            }
+//
+//        }
+//
+//        @Override
+//        public void onCancelled(DatabaseError databaseError)  {
+//
+//        }
+//    };
+
+
+
+
 
 
     }
+
+
+
 }
